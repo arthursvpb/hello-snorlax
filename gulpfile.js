eb425d8556dd68a17edcd0a52b2c4622f7a8de6d
arthursvpb <arthurandroid15@gmail.com>
2021-09-20 18:27:46 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+
+const {
+  src, dest, series, watch,
+} = require('gulp');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const { sync, logError } = require('gulp-sass')(require('node-sass'));
+
+function style(cb) {
+  src('src/style/**/*.scss')
+    .pipe(sync().on('error', logError))
+    .pipe(concat('style.css'))
+    .pipe(minifyCSS())
+    .pipe(dest('dist'));
+
+  cb();
+}
+
+function scripts(cb) {
+  src('src/js/**/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'));
+
+  cb();
+}
+
+function copy(cb) {
+  src([
+    'src/assets/**/*.jpg',
+    'src/assets/**/*.png',
+    'src/assets/**/*.webp',
+  ]).pipe(dest('dist'));
+
+  cb();
+}
+
+exports.default = function () {
+  watch('src', series(style, scripts, copy));
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./routes');
+
+const PORT = 3000;
+const app = express();
+
+const hbs = expressHbs.create({
+  extname: '.hbs',
+});
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
