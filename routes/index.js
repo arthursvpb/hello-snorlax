a0bf5fe516e358f48e53fcc457387a39895866f5
arthursvpb <arthurandroid15@gmail.com>
2021-09-21 10:36:21 -0300

    add json import

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 
 const router = Router();
 
+const about = require('../json/about.json');
+
 router.get('/', (req, res) => {
   res.render('home', { person: { name: 'Arthur' } });
 });
 
 router.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', about);
 });
 
 module.exports = router;
